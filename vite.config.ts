f6b1400ba1b81c8262b0f4e9c7fe38161cb4b1ee
Ashwani kumar <73331636+ashwanikumar1499@users.noreply.github.com>
2025-12-17 22:09:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  // Dev server config (local only, ignored by Vercel)
   server: {
     port: 3000,
     host: "0.0.0.0",
   },
-  {
-  routes: [
-    { handle: "filesystem" },
-    { src: "/.*", "dest": "/" }
-  ]
-},
 
-
-  // Path aliases
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
